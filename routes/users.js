b5fc5eb4c1cf25cc5eae38ba95c9e0482176588d
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2016-11-12 02:21:00 -0800

    added node modules and some stub routing

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.get('/test', function(req, res, next) {
+  res.send('responding from USER route\n');
+})
+
 module.exports = router;
