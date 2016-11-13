77e8d9c388d083c61b0fd9ebaba56322bbdb8fee
Michael Qu <mzqu98@gmail.com>
2016-11-12 21:19:29 -0800

    stuff

--- public/javascripts/advue.js
+++ public/javascripts/advue.js
@@ -1,26 +1,3 @@
-var STORAGE_KEY = 'myweb-dev'
-
-var userStorage = {
-  fetch: function () {
-    var users = JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]');
-    users.forEach(function (user, index) {
-      user.id = index;
-    });
-    userStorage.uid = users.length;
-    return users;
-  },
-  save: function (users) {
-    localStorage.setItem(STORAGE_KEY, JSON.stringify(users));
-    userStorage.uid = users.length;
-  }
-}
-
-var filters = {
-  all: function(notes) {
-    return notes;
-  }
-}
-
 var ad = new Vue({
   el: '#ad',
   data: {
