f357e33167db61b76783b54c9656d4b7b2d9eb37
Arthur Silverstein(bluegoo192) <silversteinarthur@gmail.com>
2017-04-05 09:57:32 -0700

    made already-logged-in redirect take you to the /userhome RELATIVE path, not eadmiralads.azurewebsites.net

--- public/javascripts/appvue.js
+++ public/javascripts/appvue.js
@@ -6,7 +6,7 @@ var app = new Vue({
   created: function () {
     if (userStorage.fetch()[0]) {
       console.log("user logged");
-      window.location = "http://admiralads.azurewebsites.net/userhome";
+      window.location.href = "/userhome";
     }
   },
   methods: {
